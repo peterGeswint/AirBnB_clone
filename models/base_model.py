500b45f52e7a037dbfc5edeff39229f1c3e3ebd9
peterGeswint <petergeswint@outlook.com>
2024-06-05 18:47:35 -0700

    t

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,18 @@ import uuid
 from datetime import datetime
 
 
-import uuid
-from datetime import datetime
-
-
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ("created_at", "updated_at"):
+                        value = datetime.fromisoformat(value)
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
@@ -40,7 +43,22 @@ if __name__ == "__main__":
     print(my_model_json)
     for key in my_model_json:
         print(
-            "\t{}: ({}) - {}".format(key, type(my_model_json[key]),\
-                    my_model_json[key])
+            "\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key])
+        )
+
+    # Creating an instance with kwargs
+    kwargs = {
+        "id": "123",
+        "created_at": "2023-06-06T12:00:00.000000",
+        "updated_at": "2023-06-06T12:00:00.000000",
+        "name": "Test Model"
+    }
+    my_model_from_kwargs = BaseModel(**kwargs)
+    print(my_model_from_kwargs)
+    my_model_from_kwargs_json = my_model_from_kwargs.to_dict()
+    print(my_model_from_kwargs_json)
+    for key in my_model_from_kwargs_json:
+        print(
+            "\t{}: ({}) - {}".format(key, type(my_model_from_kwargs_json[key]), my_model_from_kwargs_json[key])
         )
 
